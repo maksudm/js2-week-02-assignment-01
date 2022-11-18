155de0640434033efd37541ddd7d9efc9fb97853
maksud149 <merjanovm@gmail.com>
2022-11-18 15:34:27 +0100

    2

--- functions.js
+++ functions.js
@@ -1,10 +1,34 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (arr) => {
+  let oddNumberArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 != 0) {
+      oddNumberArr.push(arr[i]);
+    }
+  }
+  return oddNumberArr.length;
+};
 
-const getIndexOfFalseItem = () => {};
+const getIndexOfFalseItem = (arr) => {
+  let index = arr.indexOf(false)
+  return index;
+};
 
-const filterNumbers = () => {};
+const filterNumbers = (arr) => {
+  let result = arr.filter(elem => {
+    return typeof elem == 'number';
+  })
+  return result;
+};
 
-const generateEmailAddress = () => {};
+const generateEmailAddress = (arr) => {
+  let emailArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let email = arr[i].toLowerCase() + '@gmail.com';
+    emailArr.push(email);
+
+  }
+  return emailArr;
+};
 
 export {
   findTheNumberOfOddNumbers,
